93ae8b5e100596b05c36e16c72f72ae1d0d6d18c
HeadsShot <41201881+Starfruit2210@users.noreply.github.com>
2026-03-22 22:03:13 +0700

    feat: added function to add command per product

--- src/main/java/me/aglerr/donations/managers/ProductManager.java
+++ src/main/java/me/aglerr/donations/managers/ProductManager.java
@@ -1,16 +1,14 @@
 package me.aglerr.donations.managers;
 
-import com.muhammaddaffa.mdlib.utils.Common;
 import com.muhammaddaffa.mdlib.utils.Logger;
 import me.aglerr.donations.DonationPlugin;
 import me.aglerr.donations.objects.Product;
-import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
 import java.util.*;
 
 public class ProductManager {
@@ -63,41 +61,43 @@ public class ProductManager {
 
 
     public void loadProduct(String id, ConfigurationSection section) {
-        String displayName = section.getString("displayName");
-        double price = section.getDouble("price");
+        String basePath = section.getCurrentPath(); // e.g. "products.vip"
+        List<String> errors = new ArrayList<>();
 
-        // Register all product inside
-        Product product = new Product(id, displayName, price);
-        this.productList.put(id, product);
-        Logger.info("Successfully loaded '" +id+ "'");
-    }
+        String displayName = section.getString("displayName", null);
+        if (displayName == null || displayName.isBlank()) {
+            errors.add(basePath + ".displayName is missing or empty");
+        }
 
+        Object rawPrice = section.get("price");
+        Double price = null;
+        if (rawPrice == null) {
+            errors.add(basePath + ".price is missing");
+        } else if (rawPrice instanceof Number number) {
+            price = number.doubleValue();
+            if (price < 0) errors.add(basePath + ".price must be >= 0 (got " + price + ")");
+        } else {
+            errors.add(basePath + ".price must be a number (got " + rawPrice.getClass().getSimpleName() + ")");
+        }
 
+        List<String> commandList = section.getStringList("command");
+        if (commandList == null || commandList.isEmpty()) {
+            errors.add(basePath + ".command is missing or empty (must be a list of commands)");
+        }
 
-        /*public Product getProduct(String productName){
-        return productList.stream().filter(product -> product.getName().equalsIgnoreCase(productName))
-                .findAny().orElse(null);
-        }*/
-
-
-        /*public void loadProduct(){
-        FileConfiguration config = DonationPlugin.PRODUCT_CONFIG.getConfig();
-        // Send a console message
-        Logger.info("&rLoading all products...");
-        // Return if there are no products
-        if(!config.isConfigurationSection("products")){
-            Logger.info("&cFailed, because there are no products!");
+        // If errors exist, print them and SKIP loading this product
+        if (!errors.isEmpty()) {
+            Logger.info("&cFailed to load product '" + id + "':");
+            for (String error : errors) {
+                Logger.info("&7- &c" + error);
+            }
             return;
         }
-        // If there are products, loop through the config section
-        for (String productName : config.getConfigurationSection("products").getKeys(false)){
-            String displayName = config.getString("products." + productName + ".displayName");
-            double price = config.getDouble("products." + productName + ".price");
-            // After getting all the values, store them into the list
-            productList.add(new Product(productName, displayName, price));
-            // Log to the console
-            Logger.info("&rLoaded " + productName + " product.");
-        }
-        Logger.info("&rSuccessfully loaded all products!");
-    }*/
+
+        // Safe to load now
+        Product product = new Product(id, displayName, price, commandList);
+        this.productList.put(id, product);
+        Logger.info("Successfully loaded '" + id + "'");
+    }
+
 }
